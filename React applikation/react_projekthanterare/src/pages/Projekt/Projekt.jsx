1bbf6c77291a64d319e1813591b78af845a3450e
a23lisjo <a23lisjo@student.his.se>
2026-03-16 12:27:16 +0100

    Skapade en sida för Projekt och en CSS sida, kopplade dessa och lade till funktion i jsx filen

---
+++ React applikation/react_projekthanterare/src/pages/Projekt/Projekt.jsx
@@ -0,0 +1,25 @@
+import "./Projekt.css"
+function Projekt(){
+    return(
+        <div className="page">
+            <table id="projektTabell">
+                <thead>
+                    <tr>
+                        <th>Projekt namn</th>
+                        <th>Startdatum</th>
+                        <th>Slutdaturm</th>
+                        <th>Prioritet</th>
+                    </tr>
+                </thead>
+                
+
+
+            </table>
+
+
+
+        </div>
+    )
+}
+
+export default Projekt;
