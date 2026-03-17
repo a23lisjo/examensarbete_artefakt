6ef212d3d4943dd7ecc9f42e8aae674f8db53825
a23lisjo <a23lisjo@student.his.se>
2026-03-17 14:45:39 +0100

    Ändrade class > className

--- React applikation/react_projekthanterare/src/components/NavBar.jsx
+++ React applikation/react_projekthanterare/src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/" class="navbutton">Hem</Link>
-      <Link to="/projekt" class="navbutton">Projekt</Link>
+      <Link to="/" className="navbutton">Hem</Link>
+      <Link to="/projekt" className="navbutton">Projekt</Link>
 
     </nav>
   );
