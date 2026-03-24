0bee1713dd47c2223f135ea92f7b09d603f7e086
a23lisjo <a23lisjo@student.his.se>
2026-03-24 11:31:02 +0100

    Label + prioritet

--- React applikation/react_projekthanterare/src/components/ObjektForm.jsx
+++ React applikation/react_projekthanterare/src/components/ObjektForm.jsx
@@ -1,10 +1,24 @@
 function ObjetkForm(){
     return(
         <form>
-            <input type="text" placeholder="Projekt namn" />
-            <input type="date" placeholder="Startdatum" />
-            <input type="date" placeholder="Slutdatum" />
-            <button type="submit">Lägg till</button>
+            <label>Namn:
+                <input type="text" placeholder="Projekt namn" />
+            </label>
+            <label>Startdatum:
+                <input type="date" placeholder="Startdatum" />
+            </label>
+            <label>Startdatum:
+                <input type="date" placeholder="Slutdatum" />
+            </label>
+            <label>Prioritet:
+                <select name="priority" id="priority">
+                    <option value="high">Hög</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Låg</option>
+                </select>
+            </label>
+
+            <button id="objektFormSubmitButton" type="submit">Lägg till</button>
         </form>
     )
 }
