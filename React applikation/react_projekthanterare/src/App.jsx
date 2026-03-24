d6e08ea7a7cb92ad995b6ac4ae7729682c6a6dd5
a23lisjo <a23lisjo@student.his.se>
2026-03-24 11:29:07 +0100

    Fixade grid för hela applikationen

--- React applikation/react_projekthanterare/src/App.jsx
+++ React applikation/react_projekthanterare/src/App.jsx
@@ -1,18 +1,19 @@
 import Navbar from "./components/navbar";
 import Hem from "./pages/Hem/Hem";
 import Projekt from "./pages/Projekt/Projekt";
+import "./App.css"
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div id="templatePage">
       <Navbar/>
-      <Routes>
-        <Route path="/" element={<Hem />} />
-        <Route path="/projekt" element={<Projekt />} />
-      </Routes>
-    </>
+        <Routes>
+          <Route path="/" element={<Hem />} />
+          <Route path="/projekt" element={<Projekt />} />
+        </Routes>
+    </div>
   );
 }
 
