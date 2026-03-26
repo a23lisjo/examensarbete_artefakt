c17c30a873fce7b6c67f3e5e0b33958fd6b712f5
a23lisjo <a23lisjo@student.his.se>
2026-03-26 19:29:42 +0100

    Lade till tabell och styling för formulär och tabell

--- Svelte applikation/svelte_projekthanterare/src/routes/projekt/+page.svelte
+++ Svelte applikation/svelte_projekthanterare/src/routes/projekt/+page.svelte
@@ -11,5 +11,48 @@
         <h1 id="pageTitle">Projekt</h1>
         <ObjektForm/>
     </div>
+    <table id="projektTabell">
+        <thead>
+            <tr>
+                <th>Projekt namn</th>
+                <th>Startdatum</th>
+                <th>Slutdaturm</th>
+                <th>Prioritet</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
     
 </div>
+
+<style>
+    #projektTabell{
+    border-collapse: collapse;
+    margin-top: 1em;
+}
+
+th{
+    border: 1px solid rgb(84, 84, 84);
+    background-color: #d7d9ff;
+    color:#646cff;
+}
+
+tr:nth-child(even) {background-color: #e8e8e8;
+}
+tr:hover{
+    background-color: #adadad;
+}
+
+#pageTitle{
+    color:#3038d6;
+}
+
+#addProjektSection{
+    border: 1px grey solid;
+    box-shadow: 5px 5px #d7d9ff;
+    padding: 1em;
+}
+
+</style>
