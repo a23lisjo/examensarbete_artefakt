8a2045e9ac121011753c96fb61b2d2149aa77ed8
a23lisjo <a23lisjo@student.his.se>
2026-03-26 19:18:28 +0100

    Lade till komponent för formulär för att lägga till objekt i tabell

---
+++ Svelte applikation/svelte_projekthanterare/src/lib/components/ObjektForm.svelte
@@ -0,0 +1,20 @@
+<form>
+            <label>Namn:
+                <input type="text" placeholder="Projekt namn" />
+            </label>
+            <label>Startdatum:
+                <input type="date" placeholder="Startdatum" />
+            </label>
+            <label>Startdatum:
+                <input type="date" placeholder="Slutdatum" />
+            </label>
+            <label>Prioritet:
+                <select name="priority" id="priority">
+                    <option value="high">Hög</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Låg</option>
+                </select>
+            </label>
+
+            <button id="objektFormSubmitButton" type="submit">Lägg till</button>
+        </form>
---
+++ Svelte applikation/svelte_projekthanterare/src/routes/projekt/+page.svelte
@@ -0,0 +1,15 @@
+ <script>
+    import Navbar from '$lib/components/Navbar.svelte';
+    import ObjektForm from '$lib/components/ObjektForm.svelte';
+
+ </script>
+
+ <Navbar/>
+
+ <div class="page">
+    <div id="addProjektSection">
+        <h1 id="pageTitle">Projekt</h1>
+        <ObjektForm/>
+    </div>
+    
+</div>
